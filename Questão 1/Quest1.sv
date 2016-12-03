48e2c58118e461261d94a5451290782b4e7fb106
Rodrigo Farias <rodrigofarias23d@gmail.com>
2016-12-03 00:41:05 -0300

    Resolução da primeira questão

---
+++ Questão 1/Quest1.sv
@@ -0,0 +1,21 @@
+module Quest1 (input logic swap,
+					input logic enable,
+					input logic clock,
+					output logic[3:0] upcount,
+					output logic[3:0] downcount);
+
+always_ff @(posedge clock)
+	if (enable)
+		if (swap) begin
+			upcount <= downcount;
+			downcount <= upcount;
+		end else begin
+			upcount <= upcount + 4'd1;
+			downcount <= downcount - 4'd1;
+		end
+	else begin
+		upcount <= upcount;
+		downcount <= downcount;
+	end
+
+endmodule
