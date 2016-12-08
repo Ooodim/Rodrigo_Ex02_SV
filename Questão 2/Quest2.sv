d21c618da4f0ec3cc2eab44684e0d399e84bfd1f
Rodrigo farias <rodrigofarias23d@gmail.com>
2016-12-08 21:05:29 -0200

    Alterando identação

--- Questão 2/Quest2.sv
+++ Questão 2/Quest2.sv
@@ -1,13 +1,13 @@
 module Quest2(input logic clock,
-				  output logic f);
+              output logic f);
 
 logic[8:0] count;
 
 always_ff @(posedge clock) begin
-	if (count == 9'd499) count = 9'd0;
-	else count = count + 9'd1;
-	if (count > 19 && count < 90) f = 1'b0;
-	else f = 1'b1;
+    if (count == 9'd499) count = 9'd0;
+    else count = count + 9'd1;
+    if (count > 19 && count < 90) f = 1'b0;
+    else f = 1'b1;
 end
 
 endmodule 
