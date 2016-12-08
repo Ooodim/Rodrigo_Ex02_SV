43dbcf9afc3782a1a76468fcce088ab0fbf9348d
Rodrigo farias <rodrigofarias23d@gmail.com>
2016-12-08 21:04:09 -0200

    Alterando identação

--- Questão 1/Quest1.sv
+++ Questão 1/Quest1.sv
@@ -1,21 +1,20 @@
-module Quest1 (input logic swap,
-				input logic enable,
-				input logic clock,
-				output logic[3:0] upcount,
-				output logic[3:0] downcount);
+module Quest1 (input logic swap, input logic enable,
+               input logic clock,
+               output logic[3:0] upcount,
+               utput logic[3:0] downcount);
 
 always_ff @(posedge clock)
-	if (enable)
-		if (swap) begin
-			upcount <= downcount;
-			downcount <= upcount;
-		end else begin
-			upcount <= upcount + 4'd1;
-			downcount <= downcount - 4'd1;
-		end
-	else begin
-		upcount <= upcount;
-		downcount <= downcount;
-	end
+    if (enable)
+        if (swap) begin
+            upcount <= downcount;
+            downcount <= upcount;
+        end else begin
+            upcount <= upcount + 4'd1;
+            downcount <= downcount - 4'd1;
+        end
+    else begin
+        upcount <= upcount;
+        downcount <= downcount;
+    end
 
-endmodule
+endmodule 
