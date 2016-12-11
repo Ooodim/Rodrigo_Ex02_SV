34b3325b6aa7445cacc89c985d6aaa1798b7c952
Rodrigo farias <rodrigofarias23d@gmail.com>
2016-12-11 19:17:08 -0200

    Correção no código

--- Questão 2/Quest2.sv
+++ Questão 2/Quest2.sv
@@ -6,7 +6,7 @@ logic[8:0] count;
 always_ff @(posedge clock) begin
   if (count == 9'd499) count <= 9'd0;
     else count <= count + 9'd1;
-  if (count > 19 && count < 90) f <= 1'b0;
+  if (count > 9'd19 && count < 9'd90) f <= 1'b0;
     else f <= 1'b1;
 end
 
